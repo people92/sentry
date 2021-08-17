c21eb1e810b94f515adbc2066722959ce78f990b
people92 <eminjae8659@gmail.com>
2021-08-17 10:03:05 +0900

    create

---
+++ src/main/java/com/people/sentry/SentryApplication.java
@@ -0,0 +1,13 @@
+package com.people.sentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentryApplication.class, args);
+    }
+
+}
